594064c3dee6037b2bfa11bbbe3b97d9476d7e3f
bthn-arsln <bthnarsln64@gmail.com>
2021-01-16 23:42:47 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array - dizi
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirici Yetştirme Kampı",
+                "Programlamaya başlangıç için temel kurs",
+                "Java",
+                "Python"
+            };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+            foreach(string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+        }
+    }
+}
