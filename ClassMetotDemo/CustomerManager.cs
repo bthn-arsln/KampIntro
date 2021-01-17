68fef44c4e9bca3a446526da7519197ad18f0a2d
Batuhan Arslan <bthnarsln64@gmail.com>
2021-01-17 19:27:35 +0300

    müşteriler eklendi listelendi ve silindi

--- ClassMetotDemo/CustomerManager.cs
+++ ClassMetotDemo/CustomerManager.cs
@@ -6,9 +6,21 @@ namespace ClassMetotDemo
 {
     class CustomerManager
     {
-        public void Add()
+        public void Add(Customer customer)
         {
-
+            Console.WriteLine("Müşteri eklendi: " + customer.FirstName + " " + customer.LastName);
+        }
+        public void getCustomers(Customer[] customers)
+        {
+            Console.WriteLine("Müşteriler listelendi");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Müşeteri Adı: " + customer.FirstName + " Müşteri Soyadı: " + customer.LastName);
+            }
+        }
+        public void deleteCustomer(int id)
+        {
+            Console.WriteLine(id + " id li müşteri silindi");
         }
     }
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,8 +6,33 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.Write("Adınız: ");
-            string ad = Console.ReadLine();
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "ad1";
+            customer1.LastName = "soyad1";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "ad2";
+            customer2.LastName = "soyad2";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "ad3";
+            customer3.LastName = "soyad3";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            customerManager.getCustomers(customers);
+
+            customerManager.deleteCustomer(customer1.Id);
+            customerManager.deleteCustomer(customer2.Id);
+            customerManager.deleteCustomer(customer3.Id);
         }
     }
 }
