40e348a3ef23e9e3dfcdd77875822ec3d66ba14a
Batuhan Arslan <bthnarsln64@gmail.com>
2021-01-21 20:34:39 +0300

    Dictionary ödevi bitti

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DictionaryIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> sayilar = new MyDictionary<string, int>();
+            sayilar.Add("sayi1", 1);
+            sayilar.Add("sayi2", 2);
+            sayilar.Add("sayi3", 3);
+            sayilar.Add("sayi4", 4);
+
+            foreach (var key in sayilar.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach (var value in sayilar.Values)
+            {
+                Console.WriteLine(value);
+            }
+           
+        }
+    }
+}
